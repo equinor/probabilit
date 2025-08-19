1138279115cd86fd2c4d40f3015c0e165f2405f7
Tommy Odland <todl@equinor.com>
2025-08-19 13:21:42 +0200

    added correlations

--- src/probabilit/read_yaml_create_samples.py
+++ src/probabilit/read_yaml_create_samples.py
@@ -2,39 +2,51 @@ from probabilit.modeling import Distribution, Add
 import yaml
 from yaml.loader import Loader
 import pandas as pd
+import numpy as np
+import scipy as sp
 
-file = "config.yml"
+if __name__ == "__main__":
+    file = "config.yml"
 
+    # Load data from file into a dictionary
+    with open(file, "r") as file_handle:
+        yaml_data = yaml.load(file_handle, Loader)
 
-# Load data from file into a dictionary
-with open(file, "r") as file_handle:
-    yaml_data = yaml.load(file_handle, Loader)
-    
-# Load sections of the dictionary as variables
-metadata = yaml_data["metadata"]
-variables = yaml_data["variables"]
+    # Load sections of the dictionary as variables
+    metadata = yaml_data["metadata"]
+    variables = yaml_data["variables"]
+    correlations = yaml_data.get("correlations", [])
 
-# Convert dict of {name:data, ...} to {name:Distribution, ...}
-for variable_name in variables.keys():
-    variable_data = variables[variable_name]
-    variables[variable_name] = Distribution(variable_data["type"],
-                            **variable_data["parameters"])
-    
-    
-# Dummy expression to sample all parents
-expression = Add(*variables.values())
-expression.sample(size=metadata["samples"])
+    # Convert dict of {name:data, ...} to {name:Distribution, ...}
+    for varname in variables.keys():
+        vardata = variables[varname]
+        variables[varname] = Distribution(vardata["type"], **vardata["parameters"])
 
-# Collect all samples into a dataframe
-df_samples = pd.DataFrame({name:distr.samples_ for (name, distr) in variables.items()})
+    # Dummy expression to sample all parents
+    expression = Add(*variables.values())
 
-print(df_samples)
+    # Add every correlation pair
+    # NOTE if we define correlations between (a, b) and (c, d)
+    # then the non-specified correlations (e.g. (a, c)) will be optimized
+    # towards zero.
+    for correlation in correlations:
+        value, (varname1, varname2) = next(iter(correlation.items()))
 
+        # Prepare input
+        corr_mat = sp.linalg.circulant([1.0, value])
+        var1, var2 = variables[varname1], variables[varname2]
 
+        # Add correlation pair
+        expression.correlate(var1, var2, corr_mat=corr_mat)
 
+    # Samle the expression, which populates `.samples_` on Distributions
+    expression.sample(size=metadata["samples"])
 
+    # Collect all samples into a dataframe
+    df_samples = pd.DataFrame(
+        {name: distr.samples_ for (name, distr) in variables.items()}
+    )
 
+    print(df_samples)
 
-    
-    
-    
+    print(df_samples.corr())
