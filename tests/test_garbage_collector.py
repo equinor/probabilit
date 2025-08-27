0be504abe6d2ac21fc86a6fd84dcdc4a00b6d82c
Tommy Odland <todl@equinor.com>
2025-08-27 11:17:23 +0200

    wrote a larger test

--- tests/test_garbage_collector.py
+++ tests/test_garbage_collector.py
@@ -38,14 +38,34 @@ def test_garbage_collector():
     assert hasattr(the_result, "samples_")
 
     # Partial garbage collection => selected nodes have samples_
-    the_result.sample(99, random_state=42, gc_strategy=[scale, b])
-    assert not hasattr(loc, "samples_")
+    the_result.sample(99, random_state=42, gc_strategy=[loc, scale])
+    assert hasattr(loc, "samples_")
     assert hasattr(scale, "samples_")
     assert not hasattr(a, "samples_")
-    assert hasattr(b, "samples_")
+    assert not hasattr(b, "samples_")
     assert not hasattr(the_sum, "samples_")
     assert not hasattr(the_power, "samples_")
     assert hasattr(the_result, "samples_")
+
+
+def test_garbage_collector_large_graph():
+    sampling_nodes = []
+
+    result = 0
+    for year in range(99):
+        addition = Distribution("norm", loc=100, scale=10)
+        interest_rate = Distribution("norm", loc=1.05, scale=0.05)
+        result = result * interest_rate + addition
+
+        # Store nodes to check them later
+        sampling_nodes.append(addition)
+        sampling_nodes.append(interest_rate)
+
+    result.sample(99, random_state=42, gc_strategy=[])
+
+    # Verify that GC worked
+    assert hasattr(result, "samples_")
+    assert not any(hasattr(n, "samples_") for n in sampling_nodes)
 
 
 if __name__ == "__main__":
