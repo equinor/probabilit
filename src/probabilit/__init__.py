9f1e64824a43031636622ee226586829b931868b
Tommy Odland <todl@equinor.com>
2025-08-28 12:48:06 +0200

    added some module level imports in __init__

---
+++ src/probabilit/__init__.py
@@ -0,0 +1,24 @@
+from probabilit.modeling import (
+    Distribution,
+    Constant,
+    EmpiricalDistribution,
+    CumulativeDistribution,
+    DiscreteDistribution,
+    Equal,
+    scalar_transform,
+    MultivariateDistribution,
+)
+from probabilit.distributions import PERT
+
+
+__all__ = [
+    "Distribution",
+    "Constant",
+    "EmpiricalDistribution",
+    "CumulativeDistribution",
+    "DiscreteDistribution",
+    "Equal",
+    "scalar_transform",
+    "MultivariateDistribution",
+    "PERT",
+]
