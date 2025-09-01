5e4bfb7325aea0e467773ed4371c95fb0393854c
Tommy Odland <todl@equinor.com>
2025-09-01 10:11:54 +0200

    ruff

--- tests/test_permutation_correlator.py
+++ tests/test_permutation_correlator.py
@@ -3,9 +3,8 @@ import numpy as np
 import pytest
 import scipy as sp
 
+
 class TestPermutationCorrelator:
-    
-    
     @pytest.mark.parametrize("seed", range(25))
     def test_recommended_parameters(self, seed):
         rng = np.random.default_rng(seed)
@@ -19,13 +18,12 @@ class TestPermutationCorrelator:
         X = rng.normal(size=(n_observations, n_variables))
 
         # Tranform the data
-        transform = PermutationCorrelator(seed=seed).set_target(desired_corr)        
+        transform = PermutationCorrelator(seed=seed).set_target(desired_corr)
         X_transformed = transform(X)
-        
-        rel_err = (transform._error(X_transformed) / transform._error(X))
+
+        rel_err = transform._error(X_transformed) / transform._error(X)
         assert rel_err < 0.1
-    
-    
+
     @pytest.mark.parametrize("seed", range(1))
     def test_marginals_and_correlation_distance(self, seed):
         rng = np.random.default_rng(seed)
@@ -39,9 +37,7 @@ class TestPermutationCorrelator:
         X = rng.normal(size=(n_observations, n_variables))
 
         # Tranform the data
-        transform = PermutationCorrelator(seed=0, 
-                                          iterations=50,
-                                          max_iter_no_change=10)
+        transform = PermutationCorrelator(seed=0, iterations=50, max_iter_no_change=10)
         transform = transform.set_target(desired_corr)
         X_transformed = transform(X)
 
@@ -51,7 +47,7 @@ class TestPermutationCorrelator:
         for j in range(X.shape[1]):
             assert np.allclose(np.sort(X[:, j]), np.sort(X_transformed[:, j]))
 
-        # After using the PermutationCorrelator, the distance to the 
+        # After using the PermutationCorrelator, the distance to the
         # desired correlation matrix should be smaller than it was before.
         X_corr = np.corrcoef(X, rowvar=False)
         distance_before = sp.linalg.norm(X_corr - desired_corr, ord="fro")
@@ -60,18 +56,16 @@ class TestPermutationCorrelator:
         distance_after = sp.linalg.norm(X_trans_corr - desired_corr, ord="fro")
 
         assert distance_after <= distance_before
-    
-    
+
     def test_dataset_with_more_variables_than_observations(self):
-        
         rng = np.random.default_rng(42)
         X = rng.normal(size=(5, 10))
-        
+
         desired_corr = np.identity(10)
         transform = PermutationCorrelator(seed=0).set_target(desired_corr)
         X_trans = transform(X)
         assert transform._error(X_trans) < transform._error(X)
-    
+
     def test_dataset_with_unity_correlation_in_ranks(self):
         # This dataset is interesting because while the correlation
         # between the variables is ~0.6, when the data is ranked the
@@ -82,10 +76,8 @@ class TestPermutationCorrelator:
 
         transform = PermutationCorrelator(seed=0).set_target(desired_corr)
         X_trans = transform(X)
-        assert np.allclose(X_trans, np.array([[1. , 1.1],
-               [2.1, 1. ],
-               [2. , 3. ]]))
-        
-        
+        assert np.allclose(X_trans, np.array([[1.0, 1.1], [2.1, 1.0], [2.0, 3.0]]))
+
+
 if __name__ == "__main__":
     pytest.main(args=[__file__, "--doctest-modules", "-v", "-l"])
