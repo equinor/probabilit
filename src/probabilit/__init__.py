76c3a84657e9d3a74f3f70085417d2af1fd7ea2f
tommyod <tommy.odland>
2025-10-08 06:46:13 +0200

    Export more in __init__

--- src/probabilit/__init__.py
+++ src/probabilit/__init__.py
@@ -5,22 +5,50 @@ from probabilit.modeling import (
     CumulativeDistribution,
     DiscreteDistribution,
     Equal,
+    Exp,
+    Log,
+    Min,
+    Max,
+    All,
+    Any,
     scalar_transform,
     MultivariateDistribution,
 )
-from probabilit.distributions import PERT
-from probabilit.inspection import plot
+from probabilit.distributions import (
+    PERT,
+    Triangular,
+    Normal,
+    Lognormal,
+    Uniform,
+    TruncatedNormal,
+)
+from probabilit.inspection import plot, treeprint
 
 
 __all__ = [
+    # Geneal modeling
     "Distribution",
     "Constant",
     "EmpiricalDistribution",
     "CumulativeDistribution",
     "DiscreteDistribution",
     "Equal",
+    "Exp",
+    "Log",
+    "Min",
+    "Max",
+    "All",
+    "Any",
     "scalar_transform",
     "MultivariateDistribution",
+    # Custom distributions
     "PERT",
+    "Triangular",
+    "Normal",
+    "Lognormal",
+    "Uniform",
+    "TruncatedNormal",
+    # Inspection
     "plot",
+    "treeprint",
 ]
